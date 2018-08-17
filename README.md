58ba9d7891bd81608d2189acb7f3efb2460e266f
Rodrigo Dechen <mestri.rodrigo@gmail.com>
2018-08-17 17:42:06 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# REST
+
+Pequena clase para criar use server rest
+
+## Server
+````php
+$_HEAD = [
+    // Obrigario
+    'verbo' => 'METHOD', // GET | POST | PUT | DELETE
+    
+    // Opcional
+    'login' => $_SERVER['PHP_AUTH_USER'],
+    'password' => $_SERVER['PHP_AUTH_PW'],
+];
+
+$_DATA = [];
+
+$api = '\Teste\Teste';
+
+$api = new $api();
+
+$apiReturn = $api->request($_HEAD, $_DATA);
+````
+
+## Comando
+
+````php
+namespace Teste;
+use Rest\Rest;
+
+class Teste extends Rest{
+    
+    // confere o login do usuario
+    protected function auth($_HEAD, $_DATA = []){
+    
+        // $_HEAD['login'] 
+        // $_HEAD['password']
+         
+        return true;
+    }
+
+    protected function POST($_DATA){
+        // Cria o registro no sistemna
+    }
+}
+
+````
---
+++ src/rest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Rest;
+
+abstract class Rest{
+
+    protected $head = [];
+    private $status = 200;
+
+    final protected function status($status = null){
+        if($status === null)
+            return $this->status;
+        return ($this->status = $status);
+    }
+
+    /**
+     * @param array $_HEAD
+     * @param array $_DATA
+     * @return llRequest\Response
+     */
+    final public function request($_HEAD, $_DATA){
+        $this->head = $_HEAD;
+        $r = new \llRequest\Response();
+
+        if(!method_exists(get_called_class(), $_HEAD['verbo']))
+            throw new \Exception('Este verbo não foi implantado');
+
+        $ref = new \ReflectionMethod(get_called_class(), $_HEAD['verbo']);
+        if($ref->isProtected() && !$this->auth($_HEAD, $_DATA))
+            throw new \Exception('Este metodo requer permição de acesso');
+
+        /** @var \llRequest\Response $data */
+        $data = $this->{$_HEAD['verbo']}($_DATA);
+
+        if($data instanceof \llRequest\Response) return $data;
+        else return $r->meta((''), $this->status())->data($data);
+    }
+
+    /**
+     * @param array $_HEAD
+     * @param array $_DATA
+     * @return bool
+     */
+    protected function auth($_HEAD, $_DATA = []){
+        return true;
+    }
+
+    public function GET($_DATA){
+        throw new \Exception('Este verbo não foi implantado');
+    }
+
+    public function POST($_DATA){
+        throw new \Exception('Este verbo não foi implantado');
+    }
+
+    public function PUT($_DATA){
+        throw new \Exception('Este verbo não foi implantado');
+    }
+
+    public function DELETE($_DATA){
+        throw new \Exception('Este verbo não foi implantado');
+    }
+
+}
